6ce4a4b73a8fb0b79ba0389deaf1149afb766d7d
Valentin <valentinaubertin@outlook.fr>
2021-09-16 12:47:53 +0200

    [deleting controller]

--- conf/dependencies.php
+++ conf/dependencies.php
@@ -1,19 +1,1 @@
 <?php
-
-$container = $app->getContainer();
-
-//Twig
-$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__ . '/../src/views', $container['twig_settings']);
-    $env = $view->getEnvironment();
-
-    $view->addExtension(new \Twig\Extension\DebugExtension());
-    $view->addExtension(new Slim\Views\TwigExtension(
-        $container->router,
-        $container['request']->getUri()
-    ));
-
-    // adding global param to twig views
-    // $env->addGlobal('name', $container->name);
-    return $view;
-};
--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,7 @@
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->get('/',function ($request, $response) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request,Response $response){
+    $response->getBody()->write("hello!");
     return $response;
 });
