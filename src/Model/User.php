9228c317a4857a5e4aff115b01c4dc9c401c32c2
Hugo Fresnel <hugo.fresnel1@etu.univ-lorraine.fr>
2021-09-23 09:44:02 +0200

    update the user model to add properties

--- src/Model/User.php
+++ src/Model/User.php
@@ -6,5 +6,62 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    /**
+     * Set the corresponding table name
+     *
+     * @var string
+     */
     protected $table = 'users';
+
+    /**
+     * Disable default timestamps columns
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $firstname;
+
+    /**
+     * @var string
+     */
+    public $lastname;
+
+    /**
+     * @var int
+     */
+    public $age;
+
+    /**
+     * @var string
+     */
+    public $phone;
+
+    /**
+     * @var string
+     */
+    public $location;
+
+    /**
+     * @var bool
+     */
+    public $contamined;
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var string
+     */
+    public $password;
 }
