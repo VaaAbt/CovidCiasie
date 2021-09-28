43dceb4c069557a0953bbaa8beaa6ff6fd0be63e
Hugo Fresnel <hugo.fresnel1@etu.univ-lorraine.fr>
2021-09-28 09:57:58 +0200

    fix: quote identifiers for reserved words

--- CREATE_TABLES.sql
+++ CREATE_TABLES.sql
@@ -1,62 +1,67 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  lastname varchar(255) NOT NULL,
-  firstname varchar(255) NOT NULL,
-  age int(4) NOT NULL,
-  phone varchar(14) NOT NULL,
-  location varchar(255) NOT NULL,
-  contamined tinyint(1) NOT NULL DEFAULT '0',
-  email varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS users
+(
+    id int(11) NOT NULL AUTO_INCREMENT,
+    lastname varchar(255) NOT NULL,
+    firstname varchar(255) NOT NULL,
+    age int(4) NOT NULL,
+    phone varchar(14) NOT NULL,
+    location varchar(255) NOT NULL,
+    contamined tinyint(1) NOT NULL DEFAULT '0',
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS groups;
-CREATE TABLE IF NOT EXISTS groups (
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE IF NOT EXISTS `groups`
+(
     id int(11) NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS messages;
-CREATE TABLE IF NOT EXISTS messages (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  sender_id int(11) NOT NULL,
-  receiver_id int(11),
-  group_id int(11),
-  message varchar(255) NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_users1_id_messages
-  FOREIGN KEY (sender_id) REFERENCES users(id),
-  CONSTRAINT fk_users2_id_messages
-  FOREIGN KEY (receiver_id) REFERENCES users(id),
-  CONSTRAINT fk_groups_id_messages
-  FOREIGN KEY (group_id) REFERENCES groups(id)
+CREATE TABLE IF NOT EXISTS messages
+(
+    id int(11) NOT NULL AUTO_INCREMENT,
+    sender_id int(11) NOT NULL,
+    receiver_id int(11),
+    group_id int(11),
+    message varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_users1_id_messages
+        FOREIGN KEY (sender_id) REFERENCES users (id),
+    CONSTRAINT fk_users2_id_messages
+        FOREIGN KEY (receiver_id) REFERENCES users (id),
+    CONSTRAINT fk_groups_id_messages
+        FOREIGN KEY (group_id) REFERENCES `groups` (id)
 );
 
 
 DROP TABLE IF EXISTS groups_users;
-CREATE TABLE IF NOT EXISTS groups_users (
-  group_id int(11) NOT NULL,
-  user_id int(11) NOT NULL,
-  PRIMARY KEY(group_id, user_id),
-  CONSTRAINT fk_groups_users_id
-  FOREIGN KEY (group_id) REFERENCES groups(id),
-  CONSTRAINT fk_users_groups_id
-  FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE IF NOT EXISTS groups_users
+(
+    group_id int(11) NOT NULL,
+    user_id int(11) NOT NULL,
+    PRIMARY KEY (group_id, user_id),
+    CONSTRAINT fk_groups_users_id
+        FOREIGN KEY (group_id) REFERENCES `groups` (id),
+    CONSTRAINT fk_users_groups_id
+        FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 
 DROP TABLE IF EXISTS contacts;
-CREATE TABLE IF NOT EXISTS contacts (
+CREATE TABLE IF NOT EXISTS contacts
+(
     user1_id int(11) NOT NULL,
     user2_id int(11) NOT NULL,
-    PRIMARY KEY(user1_id, user2_id),
+    PRIMARY KEY (user1_id, user2_id),
     CONSTRAINT fk_users1_id_contacts
-    FOREIGN KEY (user1_id) REFERENCES users(id),
+        FOREIGN KEY (user1_id) REFERENCES users (id),
     CONSTRAINT fk_users2_id_contacts
-    FOREIGN KEY (user2_id) REFERENCES users(id)
+        FOREIGN KEY (user2_id) REFERENCES users (id)
 );
-    
+
 COMMIT;
