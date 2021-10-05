66bf85d73f5bf79b4252b9767627657263d502ff
leopold0906 <leopoldln55@gmail.com>
2021-10-05 08:14:32 +0200

    Add MapController with functions mapView and getOtherLocations

---
+++ src/Controller/MapController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class MapController extends AbstractController
+{
+
+    public function mapView(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        return $this->render($response, 'map.html.twig');
+    }
+
+    public function getOtherLocations(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $locations = User::getAllLocations();
+        refresh("Refresh:0");
+    }
+
+}
