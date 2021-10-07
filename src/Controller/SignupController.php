cab3a6fa9ff99b471fcb87bd74b3a8186dfa2bb6
VaaAbt <valentinaubertin@outlook.fr>
2021-10-07 10:11:56 +0200

    feat: added Respect/Validator to validate forms

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use App\Model\User;
+use Respect\Validation\Validator as v;
 
 class SignupController extends AbstractController
 {
@@ -14,11 +15,28 @@ class SignupController extends AbstractController
     }    
     public function signupUSer(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $data = $request->getParsedBody(); 
-        if($data['password'] == $data["password-c"]){
-            User::create($data);
+        $data = [
+            'firstname' => $request->getParsedBody()['firstname'],
+            'lastname' => $request->getParsedBody()['lastname'],
+            'email' => $request->getParsedBody()['email'],
+            'password' => $request->getParsedBody()['password'],
+            'password-c' => $request->getParsedBody()['password-c'],
+        ];
+
+       $dataValidator = [ 
+           'firstname' => v::notEmpty()->stringType()->validate($data['firstname']),
+           'lastname' => v::notEmpty()->stringType()->validate($data['lastname']),
+           'email' => v::notEmpty()->email()->validate($data['email']),
+           'password' => v::identical($data['password'])->validate($data['password-c'])
+        ];
+
+        $result = (bool) array_product($dataValidator); 
+
+        if($result){
+            User::create($request->getParsedBody());
             return $this->render($response, 'login.html.twig');
         }
+
         return $this->render($response, 'signup.html.twig');
     }
 }
