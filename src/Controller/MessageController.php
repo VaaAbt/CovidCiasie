f4ed7c37812d726074a136d2d39cd062b3f7d1cb
Hugo Fresnel <hugo.fresnel1@etu.univ-lorraine.fr>
2021-10-21 11:25:01 +0200

    feat: display group members in the view

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -7,6 +7,7 @@ use App\Model\GroupUser;
 use App\Model\Message;
 use App\Model\User;
 use App\Utils\Auth;
+use App\Utils\FlashMessages;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -17,6 +18,7 @@ class MessageController extends AbstractController
         $id = Auth::getUser()->getAttribute('id');
         $data['users'] = User::getTalkedToUser();
         $data['groups'] = GroupUser::getGroupsOfUser($id);
+
         return $this->render($response, 'messages/index.html.twig', $data);
     }
 
@@ -25,8 +27,9 @@ class MessageController extends AbstractController
         $id = Auth::getUser()->getAttribute('id');
         $data['users'] = User::getTalkedToUser();
         $data['groups'] = GroupUser::getGroupsOfUser($id);
+
         $data['messages'] = Message::getDiscussionMessages($args['id']);
-        $data['chatName'] = User::getUserFirstname($args['id']);
+
         return $this->render($response, 'messages/direct.html.twig', $data);
     }
 
@@ -43,22 +46,34 @@ class MessageController extends AbstractController
 
     public function getGroupMessagesView(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
+        /** @var Group|null $group */
+        $group = Group::query()->where('id', '=', $args['id'])->first();
+        if (!$group) {
+            FlashMessages::set('messages', 'Group not found.');
+            return $response->withHeader('Location', '/messages')->withStatus(404);
+        }
+
         $id = Auth::getUser()->getAttribute('id');
         $data['users'] = User::getTalkedToUser();
         $data['groups'] = GroupUser::getGroupsOfUser($id);
-        $data['messages'] = Message::getGroupDiscussionMessages($args['id']);
-        $data['chatName'] = Group::getGroupName($args['id']);
+
+        $data['group'] = $group;
+        $data['messages'] = $group->messages()->get();
+        $data['members'] = $group->users()->get();
+
         return $this->render($response, 'messages/group.html.twig', $data);
     }
 
     public function createGroupMessage(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $payload = $request->getParsedBody();
+        
         $message = [
             'group_id' => $args['id'],
             'message' => $payload['message']
         ];
         Message::create($message);
+
         return $response->withHeader('Location', '/messages/group/' . $args['id'])->withStatus(302);
     }
 }
