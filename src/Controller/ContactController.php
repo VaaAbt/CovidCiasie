225395cde7fea9f454615ed4dfe879df738d07bb
Hugo Fresnel <hugo.fresnel1@etu.univ-lorraine.fr>
2021-10-30 17:16:52 +0200

    feat: send the first message to a contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Model\Contact;
-use App\Model\GroupUser;
 use App\Model\Invitation;
 use App\Utils\Auth;
 use Psr\Http\Message\ResponseInterface;
@@ -13,10 +12,8 @@ class ContactController extends AbstractController
 {
     public function getView(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $id = Auth::getUser()->getAttribute('id');
         $data['contacts'] = Contact::getContacts();
         $data['invitations'] = Invitation::getAllInvitations();
-        $data['groups'] = GroupUser::getGroupsOfUser($id);
 
         return $this->render($response, 'contacts/contacts.html.twig', $data);
     }
